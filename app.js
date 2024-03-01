c1a3710652fb8820ed217e8402b69c92b89ab4c0
b1winner <43081501+b1winner@users.noreply.github.com>
2024-03-01 14:13:26 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            name: 'Mike',
+            course: 'CMPSC421',
+            weather: 'Cold'
+        }
+    }, methods: {
+
+    }
+}).mount("#app");
